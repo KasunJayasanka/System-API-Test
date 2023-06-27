59d6dde11a1d94428c63500021eb69563f9435ac
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-06-27 17:45:08 +0530

    API Call using PHP Script

---
+++ dbwrite.php
@@ -0,0 +1,121 @@
+<?php
+
+// Change port accordingly
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "mobileappdb_test";
+// REPLACE with Database user
+$username = "nodemcu_user";
+// REPLACE with Database user password
+$password = "";
+
+// Keep this API Key value to be compatible with the ESP32 code provided in the project page.
+// If you change this value, the ESP32 sketch needs to match
+$api_key_value = 'tPmAT5Ab3j7F9';
+
+// keep the respnse payload
+$response = ['success' => false, 'massage' => 'Internal server error' ];
+// set the http response code
+$responseCode = 500; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+    
+    // get input json payload
+    $json = file_get_contents('php://input');
+    $json = json_decode($json, true);
+
+    // get api key
+    $apiKey = $json['X-API-KEY'] ?? null; // shorthand operator IF X-API-KEY exist in header assing X-API-KEY value ELSE assign null
+
+
+    if($apiKey == $api_key_value) {
+        
+
+        // var_dump($json); return;
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Check connection
+        if ($conn->connect_error) {
+            $response['massage'] = 'Connection failed'.$conn->connect_error;
+            $responseCode = 500; // internal server error
+        } else {
+            // prepare sql connection
+            // ref -> https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+
+            $stmt1 = $conn->prepare("INSERT INTO data (dateTimeStamp,gasLeakageDetected,flameDetected,temperatureValue,window1Status,window2Status) VALUES (?,?,?,?,?,?);");
+
+            // get values form pay load
+            $dateTime = ($json['dateTime'] ?? 0); 
+            $date = ($json['date'] ?? 0); 
+            $time = ($json['time'] ?? 0); 
+            $gasLeakageDetected = (int)($json['gasLeakageDetected'] ?? 0);
+            $flameDetected = (int)($json['flameDetected'] ?? 0); 
+            $temperatureValue = (float)($json['temperatureValue'] ?? 0); 
+            $window1Status = (int)($json['window1Status'] ?? 0); 
+            $window2Status = (int)($json['window2Status'] ?? 0);
+            $gasWeight = (float)($json['gasWeight'] ?? 0);
+
+            // bind paramiters to sql statement
+            $stmt1->bind_param(
+                'siidii',
+                $dateTime, 
+                $gasLeakageDetected, 
+                $flameDetected, 
+                $temperatureValue,
+                $window1Status,
+                $window2Status,
+            );
+
+            if ($stmt1->execute() === true) {
+                $response['success'] = true;
+                $response['massage'] = 'Data inserted to Main Table successfully';
+                $responseCode = 200; // response success
+            } else {
+                $response['massage'] = 'Error: ' . $sql .' '. $conn->error;
+                $responseCode = 500; // internal server error
+            }
+
+            $stmt1->close();
+
+            $stmt2 = $conn->prepare("INSERT INTO weight_data (gasWeight,dateStamp) VALUES (?,?);");
+
+            $stmt2->bind_param(
+                'ds',
+                $gasWeight,
+                $date,
+            );
+
+            if ($stmt2->execute() === true) {
+                $response['success'] = true;
+                $response['massage'] = 'Data inserted to Weight Table successfully';
+                $responseCode = 200; // response success
+            } else {
+                $response['massage'] = 'Error: ' . $sql .' '. $conn->error;
+                $responseCode = 500; // internal server error
+            }
+
+            $stmt2->close();
+        }
+
+        // Close the connection
+        $conn->close();
+
+    } else {
+        $response['massage'] = 'Wrong API Key provided!';
+        $responseCode = 401; // unauthorized request
+    } 
+
+
+} else {
+    $response['massage'] = 'Method Not Allowed';
+    $responseCode = 405; // Method Not Allowed
+}
+
+// send response to client
+http_response_code($responseCode);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response);
+?>
