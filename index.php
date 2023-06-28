d06c845e43c30e0e0e264242b31b9c22c6146c64
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-06-28 11:10:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -48,13 +48,27 @@
             box-shadow: inset 0 0 2000px rgba(255, 255, 255, .5);
             filter: blur(10px);
             margin: -20px;
-        }         
-            h1{
+        }
+
+        .column {
+          flex: 50%;
+        }
+        
+        .columns {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          width: 100%;
+        }
+        
+        h1{
                font-size: 40px;
-            }
+        }
+        
         p{
             margin: 60px 0 0 0;
         }
+        
   </style>
 </head>
 <body>
@@ -76,46 +90,55 @@
             //Will come handy if we later want to fetch multiple rows from the table
             while($row = $result->fetch_assoc()) { //fetch_assoc fetches a row from the result of our query iteratively for every row in our result.
                 //Returning HTML from the server to show on the webpage.
-                echo '<div class="container">';
+                                                  
                 echo '<p>System API Test</p>';
                                                   
-                echo '<p>';
-                echo '   <span class="dht-labels">dateTime = </span>';
-                echo '   <span id="#">'.$row["dateTime"].'</span>';
-                echo ' </p>';
+                echo '<div class="columns">';
+                   echo'<div class="column">';
+
+                        echo '<div class="container">';
+                
                                                   
-                echo '<p>';                               
-                echo '   <span class="dht-labels">Gas Leakage = </span>';
-                echo '   <span id="#">'.$row["gasLeakageDetected"].'</span>';
-                echo ' </p>';
-
-                echo '<p>';                               
-                echo '   <span class="dht-labels">Flame Detected = </span>';
-                echo '   <span id="#">'.$row["flameDetected"].'</span>';
-                echo ' </p>';
-
-                echo '<p>';                               
-                echo '   <span class="dht-labels">Temperature Value = </span>';
-                echo '   <span id="#">'.$row["temperatureValue"].'</span>';
-                echo ' </p>';
-
-                echo '<p>';                               
-                echo '   <span class="dht-labels">Window1 Status = </span>';
-                echo '   <span id="#">'.$row["window1Status"].'</span>';
-                echo ' </p>';
-
-                echo '<p>';                               
-                echo '   <span class="dht-labels">Window2 Status = </span>';
-                echo '   <span id="#">'.$row["window2Status"].'</span>';
-                echo ' </p>';
-                                                  
-                echo '</div>';
+                            echo '<p>';
+                            echo '   <span class="dht-labels">dateTime = </span>';
+                            echo '   <span id="#">'.$row["dateTime"].'</span>';
+                            echo ' </p>';
+                                                              
+                            echo '<p>';                               
+                            echo '   <span class="dht-labels">Gas Leakage = </span>';
+                            echo '   <span id="#">'.$row["gasLeakageDetected"].'</span>';
+                            echo ' </p>';
+            
+                            echo '<p>';                               
+                            echo '   <span class="dht-labels">Flame Detected = </span>';
+                            echo '   <span id="#">'.$row["flameDetected"].'</span>';
+                            echo ' </p>';
+            
+                            echo '<p>';                               
+                            echo '   <span class="dht-labels">Temperature Value = </span>';
+                            echo '   <span id="#">'.$row["temperatureValue"].'</span>';
+                            echo ' </p>';
+            
+                            echo '<p>';                               
+                            echo '   <span class="dht-labels">Window1 Status = </span>';
+                            echo '   <span id="#">'.$row["window1Status"].'</span>';
+                            echo ' </p>';
+            
+                            echo '<p>';                               
+                            echo '   <span class="dht-labels">Window2 Status = </span>';
+                            echo '   <span id="#">'.$row["window2Status"].'</span>';
+                            echo ' </p>';
+                                                              
+                            echo '</div>';                           
+                   echo'</div>';
+                                                
+                echo '</div>'; 
+                                                 
             }
         } else {
             echo "0 results";
         }
 
-        echo '</br>';
 
         // Query the single latest entry from the database. -> SELECT * FROM table_name ORDER BY col_name DESC LIMIT 1
         $sql2 = "SELECT * FROM weight_data ORDER BY id DESC LIMIT 1";
@@ -124,12 +147,15 @@
         if ($result2->num_rows > 0) {
             //Will come handy if we later want to fetch multiple rows from the table
             while($row2 = $result2->fetch_assoc()) { //fetch_assoc fetches a row from the result of our query iteratively for every row in our result.
-                //Returning HTML from the server to show on the webpage.                               
-                echo '<p>';
-                echo '   <span class="dht-labels">Gas Weight = </span>';
-                echo '   <span id="#">'.$row2["gasWeight"].'</span>';
-                echo ' </p>';                                                                 
-                
+                //Returning HTML from the server to show on the webpage. 
+
+                echo'<div class="column">';
+                    echo '<p>';
+                    echo '   <span class="dht-labels">Gas Weight = </span>';
+                    echo '   <span id="#">'.$row2["gasWeight"].'</span>';
+                    echo ' </p>';     
+                echo'</div>';  
+                                                    
             }
         } else {
             echo "0 results";
