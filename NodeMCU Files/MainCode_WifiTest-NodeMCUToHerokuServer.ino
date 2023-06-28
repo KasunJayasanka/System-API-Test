36389eecfe7cef43f3005eb4372e6439ec8f71a3
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-06-28 13:53:10 +0530

    Update MainCode_WifiTest-NodeMCUToHerokuServer.ino

--- NodeMCU Files/MainCode_WifiTest-NodeMCUToHerokuServer.ino
+++ NodeMCU Files/MainCode_WifiTest-NodeMCUToHerokuServer.ino
@@ -16,71 +16,18 @@ HTTPClient https;
 WiFiClient client;
 
 // preper jason payload
-String getSensorValues(String dateTime,String date,String time,String gasLeakageDetected,String flameDetected,String temperatureValue,String window1Status,String window2Status,String gasWeight) {
+String getSensorValues() {
 
+  String jsonString="";
   delay(1000);
 
-  String jsonString = "";
-  JsonObject data = doc.to<JsonObject>(); // create json object
+  if (Serial.available()>0) {
 
-  // set X-API-KEY
-  data["X-API-KEY"] = API_KEY;
+    jsonString = Serial.readStringUntil('\n');
+    Serial.println(jsonString);
 
-  data["dateTime"] = dateTime;
-  data["date"] = date;
-  data["time"] = time;
-  data["gasLeakageDetected"] = gasLeakageDetected;
-  data["flameDetected"] = flameDetected;
-  data["temperatureValue"] = temperatureValue;
-  data["window1Status"] = window1Status;
-  data["window2Status"] = window2Status;
-  data["gasWeight"] = gasWeight;
-
-  serializeJson(data, jsonString);
-
-  Serial.println(jsonString); // print to serilal monitoro for debug purpuses
-
+  }
   return jsonString;
-
-}
-
-
-String gettingValuesFromArduino(){
-    String jsonOutput;
-    
-    if (Serial.available()>0) {
-    
-      String dataString = Serial.readStringUntil('\n');
-      
-      dataString.trim();
-
-      String dateTime = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the first sensor value
-      String date = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the second sensor value
-      String time = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the third sensor value
-      String gasLeakageDetected = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the fourth sensor value
-      String flameDetected = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the fifth sensor value
-      String temperatureValue = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the sixth sensor value
-      String window1Status = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the seventh sensor value
-      String window2Status = dataString.substring(dataString.indexOf(":") + 1, dataString.indexOf(","));
-      dataString = dataString.substring(dataString.indexOf(",") + 1); // Remove the eighth sensor value
-      String gasWeight = dataString.substring(dataString.indexOf(":") + 1,dataString.indexOf(","));
-      
-      //printReceivedData(dateTime,date,time,gasLeakageDetected,flameDetected,temperatureValue,window1Status,window2Status,gasWeight);
-
-      jsonOutput= getSensorValues(dateTime,date,time,gasLeakageDetected,flameDetected,temperatureValue,window1Status,window2Status,gasWeight);
-      }
-      else
-      {
-      jsonOutput=getSensorValues("","","","","","","","","");
-      }
-      return jsonOutput;
 }
 
 void setup()
@@ -93,7 +40,7 @@ void loop()
 {
   // get data
 
-      String jsonOutput = gettingValuesFromArduino();
+      String jsonOutput = getSensorValues();
       
       // post data to server and get response
       error = deserializeJson(doc, SendDataToServer(jsonOutput));
@@ -180,20 +127,4 @@ void connectToWifi(void){
     Serial.println("connected");
 }
 
-void printReceivedData(String dateTime,String date,String time,String gasLeakageDetected,String flameDetected,String temperatureValue,String window1Status,String window2Status,String gasWeight){
 
-    String serialComStr;
-    serialComStr="dateTime:"+dateTime+",";
-    serialComStr+="date:"+date+",";
-    serialComStr+="time:"+time+",";
-    serialComStr+="gasLeakageDetected:"+gasLeakageDetected+",";
-    serialComStr+="flameDetected:"+flameDetected+",";
-    serialComStr+="temperatureValue:"+temperatureValue+",";
-    serialComStr+="window1Status:"+window1Status+",";
-    serialComStr+="window2Status:"+window2Status+",";
-    serialComStr+="gasWeight:"+gasWeight;
-
-    Serial.println(serialComStr);
-    delay(1000);
-
-}
