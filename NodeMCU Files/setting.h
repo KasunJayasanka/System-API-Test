62c26f2c89512c46273fd2334d566125bcbfe377
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-07-02 16:37:26 +0530

    Update setting.h

--- NodeMCU Files/setting.h
+++ NodeMCU Files/setting.h
@@ -2,11 +2,13 @@
     #define setting_h
     
     /************* wifi setting *******************/
-    #define SSID        "Among_US"
-    #define PASSWORD    "lakmina2055176"
+    #define SSID        "Official Black Knight"
+    #define PASSWORD    "ZigmaE#4O4"
 
     /************* Server Setting ****************/
-    #define API               "https://system-api-test-99eb183cc101.herokuapp.com/dbwrite.php"
+    #define API               "https://system-api-test-2-a10c3c5184ce.herokuapp.com/dbwrite.php"
+    //#define API               "https://lapshop.lk/api/dbwrite.php"
+    //#define API               "https://system-api-test-99eb183cc101.herokuapp.com/"
     #define API_KEY           "tPmAT5Ab3j7F9"
     #define ERROR_MSG         "{\"success\":\"false\",\"massage\":\"error\"}"
     #define ERROR_MSG_WIFI    "{\"success\":\"false\",\"massage\":\"wifi not connected\"}"
